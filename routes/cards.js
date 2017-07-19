3b1eaf2b22df398ff91b84314378c10d0bef723c
stans <stans@amaterasu.dev>
2017-07-18 23:56:53 -0400

    Restructured flash cards to use simple JSON data file

--- routes/cards.js
+++ routes/cards.js
@@ -2,12 +2,17 @@
 const express = require('express');
 const router = express.Router();
 
+//include data file
+const { data } = require('../data/flashcardData.json');
+//equiv to data = require(...).data;
+const { cards } = data; //equiv to cards = data.cards
+
 //because of how the middleware is set up in app.js
 //all traffic defaults to /routes/cards
 //so, the root route is already /routes/cards(.js)
 router.get('/', (req, res) => {
-	res.locals.prompt = "Whose buried in Grant's Tomb?";
-	res.locals.hint = "Think about whose tomb it is.";
+	res.locals.prompt = cards[0].question;
+	res.locals.hint = cards[0].hint;
 	res.render('card');
 });
 
