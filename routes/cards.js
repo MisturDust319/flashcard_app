385bb29259907b7b69d91b616de1b97a7edce179
stans <stans@amaterasu.dev>
2017-07-21 14:05:13 -0400

    added query param and modded card.pug accordingly

--- routes/cards.js
+++ routes/cards.js
@@ -12,11 +12,18 @@ const { cards } = data; //equiv to cards = data.cards
 //so, the root route is already /routes/cards(.js)
 //the :id allows the value trailing after the route
 //	to be treated like a var, stored in req.params obj
-//	
 router.get('/:id', (req, res) => {
-	res.locals.prompt = cards[req.params.id].question;
-	res.locals.hint = cards[req.params.id].hint;
-	res.render('card');
+	const {side} = req.query;
+	const {id} = req.params;
+	const text = cards[id][side];
+	const {hint} = cards[id];
+	const templateData = {
+		text, hint, side
+	};
+	//templateData.text = text
+	//templateData.hint = hint
+	//
+	res.render('card', templateData);
 });
 
 module.exports = router;
